8b3659e20274c3854c1980a1725b6c10357ff42e
Tim Richardson <tim@timrichardson.co>
2017-04-06 14:54:33 +0200

    Initial commit

---
+++ StoryboardVisitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryboardVisitDemo
+//
+//  Created by Tim Richardson on 06/04/2017.
+//  Copyright © 2017 TRCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StoryboardVisitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StoryboardVisitDemo
+//
+//  Created by Tim Richardson on 06/04/2017.
+//  Copyright © 2017 TRCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
